4f4be5b8d39036527bf4833576c65c445eba5d82
felixmielcarek <felix.mielcarek@etu.uca.fr>
2024-10-17 08:19:41 +0200

    First version of the grid

--- src/gui/gui.c
+++ src/gui/gui.c
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef struct guiElements {
 	SDL_Window* window;
@@ -17,37 +18,81 @@ int cleanExit(int result, GuiElements* guiElements) {
 	return result;
 }
 
-int exitOnError(int result, GuiElements* guiElements, char* message) {
-	fprintf(stderr, "Error: %s.\n",message);
+int exitOnError(int result, GuiElements* guiElements, char* context) {
+	fprintf(stderr, "Error: %s.\n",context);
 	return cleanExit(result, guiElements);
 }
 
-int exitOnSDLError(int result, GuiElements* guiElements, char* step) {
-	fprintf(stderr, "Error SDL [%s]: %s", step, SDL_GetError());	
+int exitOnSDLError(int result, GuiElements* guiElements, char* context) {
+	fprintf(stderr, "Error SDL [%s]: %s", context, SDL_GetError());	
 	return cleanExit(result, guiElements);
+}
+
+int setRanderDrawColor(SDL_Renderer* renderer, SDL_Color color) {
+	return SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 
+			color.a);
 }
 
 int renderEmptyGrid(GuiElements* guiElements) {
-	SDL_Color bgColor = {0, 193, 255, 255};
-	SDL_Color white = {255, 255, 255, 255};
+	SDL_Color bgColor	= {0  , 193, 255, 255};
+	SDL_Color white		= {255, 255, 255, 255};
+	SDL_Color gray1		= {215, 219, 221, 255};
+	SDL_Color gray2		= {121, 125, 127, 255};
+	bool isLastSectionGray = false;
+	int sectionSize = 120;
+	int cellSize = sectionSize/3;
+	int gridX = 100;
+	int gridY = 100;
 
 	// region: Background
-	if(SDL_SetRenderDrawColor(guiElements->renderer, bgColor.r, bgColor.g, 
-				bgColor.b, bgColor.a) != 0)
-		return exitOnSDLError(EXIT_FAILURE, guiElements, 
-				"background color creation");
-	if(SDL_RenderClear(guiElements->renderer) != 0)
-		return exitOnSDLError(EXIT_FAILURE, guiElements, 
-				"render clear");
-	// endregion
+	setRanderDrawColor(guiElements->renderer, bgColor);
+	SDL_RenderClear(guiElements->renderer);
+	// endregion : Background
 	
-	if(SDL_SetRenderDrawColor(guiElements->renderer, white.r, white.g, 
-				white.b, white.a) != 0)
-		return exitOnSDLError(EXIT_FAILURE, guiElements,
-				"background color creation");
-	SDL_Rect rect = {100, 100, 100, 100};
-	SDL_RenderFillRect(guiElements->renderer, &rect);
 
+	// region : Grid
+	for(int sectionRow=0 ; sectionRow<3 ; ++sectionRow) {
+		int sectionX = sectionRow * sectionSize + gridX;
+		for(int sectionColumn=0 ; sectionColumn<3 ; ++sectionColumn) {
+			int sectionY = sectionColumn * sectionSize + gridY;
+
+			// region : Section
+			setRanderDrawColor(guiElements->renderer, 
+					isLastSectionGray ? white : gray1);
+
+			SDL_Rect section = { 
+				sectionX, sectionY, sectionSize, sectionSize 
+			};
+
+			SDL_RenderFillRect(guiElements->renderer, &section);
+
+			// To alternate section colors
+			isLastSectionGray = !isLastSectionGray; 
+
+			for(int cellRow=0 ; cellRow<3 ; ++cellRow) {
+				for(int cellColumn=0 ; cellColumn<3 ;
+						++cellColumn) {
+					// region : Cell
+					setRanderDrawColor( 
+							guiElements->renderer, 
+							gray2);
+					SDL_Rect cell = { 
+						cellRow*cellSize + sectionX,
+						cellColumn*cellSize + sectionY,
+						cellSize, cellSize 
+					};
+
+					SDL_RenderDrawRect( 
+							guiElements->renderer,
+							&cell);
+					// endregion : Cell
+				}
+			}
+			// endregion : Section
+		}
+	}
+	// endregion : Grid
+	
     	SDL_RenderPresent(guiElements->renderer);
 
 	return 0;
@@ -73,7 +118,7 @@ int main(int argc, char *argv[]) {
 
 	renderEmptyGrid(&guiElements);
 
-	SDL_Delay(3000);
+	SDL_Delay(10000);
 
 	return cleanExit(EXIT_SUCCESS, &guiElements);
 }
